f3f5de5296651bc83b04841823d8542cf0960e2e
Gleb <maksimenkogleb@gmail.com>
2020-12-21 18:08:03 +0200

    Практическая работа 9 Исправлены импорты и DevicePK

--- src/com/netcracker/edu/inventory/model/device/impl/DevicePK.java
+++ src/com/netcracker/edu/inventory/model/device/impl/DevicePK.java
@@ -2,8 +2,6 @@ package com.netcracker.edu.inventory.model.device.impl;
 
 import com.netcracker.edu.inventory.model.device.DevicePrimaryKey;
 
-import java.util.Objects;
-
 public class DevicePK implements DevicePrimaryKey {
     private final int in;
 
